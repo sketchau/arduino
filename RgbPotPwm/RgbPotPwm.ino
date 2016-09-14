71d2aa1cdecb283bfb8ad06f1694582907da116a
sketchau <sketchau@users.noreply.github.com>
2016-09-14 21:48:06 +1000

    Add files via upload

---
+++ RgbPotPwm/RgbPotPwm.ino
@@ -0,0 +1,37 @@
+/*
+ * 3 Pot RGB PWM
+ * APurdon
+ * 2016-09-14
+ * 
+ * Influenced by: 
+ * http://www.silvinopresa.com/how-to/arduino/rgb-led-common-annode-controlled-by-potentiometers-and-arduino/
+ * Arduino Starter Kit example 4 
+ */
+
+
+// all RGB in this project have 3 colours, so to save code repetition, use an array
+int rgbPotPins[] = { A0, A1, A2 };
+int rgbPwmPins[] = { 11, 9, 10 };
+
+// variables for reading input and writing output. Not necessary but increases readability of code
+int potValue;
+int pwmValue;
+
+void setup() {
+  for (int i = 0; i < 3; i++)
+  {
+    pinMode(rgbPotPins[i], INPUT);
+    pinMode(rgbPwmPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++)
+  {
+    // small delay to let ADC settle. This could possibly be reduced
+    delay(5);
+    potValue = analogRead(rgbPotPins[i]);
+    pwmValue = map(potValue, 0, 1023, 0, 255);
+    analogWrite(rgbPwmPins[i], pwmValue);
+  }
+}
